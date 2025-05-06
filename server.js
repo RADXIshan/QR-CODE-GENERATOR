6eb091c2631b9a7c89a34c32d846404a255c4847
Ishan Roy <ishanroy3118107@gmail.com>
2025-05-06 22:39:02 +0530

    Vercel Settings

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Serve static files
 app.use(express.static('public'));
@@ -74,4 +74,4 @@ app.post('/download', async (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
-}); 
+});
